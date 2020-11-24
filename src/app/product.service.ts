21258cf775536a7b2ef7b2d69f05e31a0fd24be0
Francisco Terrazas <francisco_terrazaff@hotmail.com>
2020-11-24 14:49:43 -0600

    Album module 4 v2

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http'
 import { Album } from './album';
+import { Observable } from 'rxjs/Observable';
 
 import 'rxjs/add/operator/map';
-import { Observable } from 'rxjs';
+
 
 
 
